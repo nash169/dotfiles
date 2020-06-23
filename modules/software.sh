c137c06f272197b80577ca07ed1cf7f0853fe053
Bernardo <bernardo.fichera@gmail.com>
2020-06-23 22:46:47 +0200

    Added some software.

--- modules/software.sh
+++ modules/software.sh
@@ -8,6 +8,10 @@ package_category Accessories
 
 list=(
 # variety
+# thunar
+# thunar-archive-plugin
+# thunar-volman
+ranger
 )
 
 count=0
@@ -22,8 +26,9 @@ done
 package_category Development
 
 list=(
-# meld
+# meld # compare and merge file/directory
 neovim
+python-pynvim
 )
 
 count=0
@@ -55,7 +60,7 @@ done
 package_category Internet
 
 list=(
-chromium
+brave-bin
 qbittorrent
 youtube-dl
 )
@@ -72,7 +77,8 @@ done
 package_category Multimedia
 
 list=(
-vlc
+mpv #vlc
+spotify
 )
 
 count=0
@@ -88,6 +94,7 @@ package_category Social
 
 list=(
 telegram-desktop
+whatsapp-nativefier
 )
 
 count=0
@@ -104,6 +111,7 @@ package_category Office
 list=(
 # evince
 zotero
+zathura
 )
 
 count=0
@@ -138,9 +146,9 @@ package_category Unpack
 
 list=(
 # unace
-# unrar
+unrar
 # zip
-# unzip
+unzip
 # sharutils
 # uudeview
 # arj
@@ -188,6 +196,8 @@ exa
 zsh
 zsh-completions
 zsh-syntax-highlighting
+zsh-autosuggestions
+neofetch
 )
 
 count=0
@@ -200,3 +210,16 @@ done
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 
 ###############################################################################
+
+package_category Fonts
+
+list=(
+nerd-fonts-hack
+)
+
+count=0
+for name in "${list[@]}" ; do
+	count=$[count+1]
+	tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
+	package_install $name
+done
