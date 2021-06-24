dd074271c27b5c08ec79a1610bb752de70cda4a3
bernardo <bernardo.fichera@gmail.com>
2021-06-24 13:38:58 +0200

    Automatic configuration script.

---
+++ scripts/install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+source utils.sh
+
+
+# CONNECT
+echo "Check if internet connection is present..."
+
+ping -c 3 google.com
+if [ $? -eq  0 ]; then
+        echo "Ping Success";
+else
+        echo "Ping Failed";
+        echo "Trying to connect..."
+        wifi-menu || error "Wifi-menu not found. Connect and run the configurtion again."
+fi
+
+# CHANGE GROUP
+read -p "Choose one of the following:
+(1) Add group
+(2) Change group
+(3) Skip" input
+
+if [ $input = "1" ]; then
+    useradd -m -g wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
+	usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
+else if [ $input = "2" ]; then
+    read -p "Group to change: " oldname
+    read -p "New group name: " newname
+    
+    groupmod −n newname oldname
+fi
+
+# CHANGE USER
+read -p "Choose one of the following:
+(1) Add user
+(2) Change user
+(3) Skip" input
+
+if [ $input = "1" ]; then
+    useradd -m -g wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
+	usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
+else if [ $input = "2" ]; then
+    read -p "Username to change: " oldname
+
+    if [$oldname = "root"]; then
+        read -p "Root selected. Do you want to change pass? [y/N]: " input
+        if [ $input = "y" ] || [ $input = "Y" ]; then
+            passwd root
+        fi
+    else
+        read -p "Do you want to change the username? [y/N]: " input
+        if [ $input = "y" ] || [ $input = "Y" ]; then
+            read -p "New username: " newname
+            usermod -l $newname $oldname
+            usermod -m -d /home/$newname $newname
+        fi
+        read -p "Do you want to change the password? [y/N]: " input
+        if [ $input = "y" ] || [ $input = "Y" ]; then
+            passwd $newname
+        fi
+    fi
+fi
+
+# ADD USER TO SUDO
+read -p "Do you want to add a user to sudoers? [y/N]: " input
+if [ $input = "y" ] || [ $input = "Y" ]; then
+    pkginstall sudo sed || error "Could not install desired packages."
+    read -p "Username to add: " name
+    sed "/^root ALL=(ALL) ALL.*/a "$name" ALL=(ALL) ALL" /etc/sudoers
+fi
+
+# Set machine name
+read -p "Do you want to set the machine name? [y/N]: " input
+if [ $input = "y" ] || [ $input = "Y" ]; then
+    read -p "Machine name: " name
+    echo $name > /etc/hostname
+    echo "127.0.0.1 localhost
+    ::1     localhost
+    127.0.0.1 "$name"" > /etc/hosts
+fi
