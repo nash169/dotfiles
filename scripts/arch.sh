b1e525018706d8bac10d2142e0a3e43d30c6ee07
Bernardo <bernardo.fichera@gmail.com>
2023-01-02 19:31:54 +0100

    Testing installation script.

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Check if a package is installed
-check_package() {
+# Check package
+pkgcheck() {
     if pacman -Qi $1 &> /dev/null; then
         tput setaf 2
         echo "The package "$1" is already installed"
@@ -16,29 +16,32 @@ check_package() {
 }
 
 # Install package
-install_package() {
-	# pacman installation
-    if pacman -Ss $1 &> /dev/null; then
-        tput setaf 3
-        echo "Installing package "$1" with pacman"
-        tput sgr0
-        sudo pacman -S --noconfirm --needed $1
-    else if pacman -Qi yay &> /dev/null; then
-	# Aur helper installation
-		tput setaf 3
-        echo "Installing package "$1" with yay"
-        tput sgr0
-        yay -S --noconfirm $1
-	else
-	# Manual installation with makepkg (in case aurhelper not installed)
-		[ -f "/usr/bin/$1" ] || (
-		dialog --infobox "Installing \"$1\"..." 4 50
-		cd /tmp || exit 1
-		rm -rf /tmp/"$1"*
-		curl -sO https://aur.archlinux.org/cgit/aur.git/snapshot/"$1".tar.gz &&
-		sudo -u "$name" tar -xvf "$1".tar.gz >/dev/null 2>&1 &&
-		cd "$1" &&
-		sudo -u "$name" makepkg --noconfirm -si >/dev/null 2>&1
-		cd /tmp || return 1)
-	fi
+pkginstall() {
+	for item in "$@"; do
+		if [! pkgcheck $item]; then
+			# pacman installation
+			if pacman -Ss $item &> /dev/null; then
+				tput setaf 3
+				echo "Installing package "$item" with pacman"
+				tput sgr0
+				sudo pacman -S --noconfirm --needed $item
+			# Aur helper installation
+			else if pacman -Qi paru &> /dev/null; then
+				tput setaf 3
+				echo "Installing package "$item" with paru"
+				tput sgr0
+				paru -S --noconfirm $item
+			else
+				[ -f "/usr/bin/$1" ] || (
+				dialog --infobox "Installing \"$1\"..." 4 50
+				cd /tmp || exit 1
+				rm -rf /tmp/"$1"*
+				curl -sO https://aur.archlinux.org/cgit/aur.git/snapshot/"$1".tar.gz &&
+				sudo -u "$name" tar -xvf "$1".tar.gz >/dev/null 2>&1 &&
+				cd "$1" &&
+				sudo -u "$name" makepkg --noconfirm -si >/dev/null 2>&1
+				cd /tmp || return 1)
+			fi
+		fi
+	done
 }
