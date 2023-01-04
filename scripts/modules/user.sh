de68325ea8049bf8fd8d0ceaa5f6568f2c6e1048
Bernardo <bernardo.fichera@gmail.com>
2023-01-04 15:19:58 +0100

    Some fix.

--- scripts/modules/user.sh
+++ scripts/modules/user.sh
@@ -4,9 +4,9 @@ source scripts/utils.sh
 # ADD USER
 read -p "Insert user name (and pass): " username
 if id "$username" &>/dev/null; then
-    echo 'User already exists'
+    echo $username
 else
     adduser $username wheel || "Error: could not add user."
     addsudo $username || "Error: could not add user to sudoers."
+    echo $username
 fi
-echo $username
