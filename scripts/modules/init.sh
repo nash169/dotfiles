8a21af18e809db4a95abdefe5f8007d25c778407
Bernardo <bernardo.fichera@gmail.com>
2023-01-04 15:05:46 +0100

    Minors.

--- scripts/modules/init.sh
+++ scripts/modules/init.sh
@@ -2,7 +2,8 @@
 source scripts/distros/$1.sh
 
 # Refresh arch keyring
-refreshkeys || "Error: could not refresh keys."
+pacman --noconfirm -Sy archlinux-keyring >/dev/null 2>&1
+# refreshkeys || "Error: could not refresh keys."
 
 # Install base packages
 base=(sudo sed curl stow)
--- scripts/modules/terminal.sh
+++ scripts/modules/terminal.sh
@@ -3,5 +3,5 @@ source scripts/distros/$1.sh
 source scripts/utils.sh
 
 gitmakeinstall $2 https://github.com/nash169/st.git || "Error: could not install TERMINAL packages."
-terminal=(tmux exa nerd-fonts-hack)
+terminal=(tmux exa nerd-fonts-hack-complete-git)
 pkginstall $2 ${terminal[@]} || "Error: could not install TERMINAL packages."
