1b2986c7370812dd13ef3188b1f7e24dd9eeb60a
Bernardo <bernardo.fichera@gmail.com>
2023-01-04 13:54:37 +0100

    Testing.

--- install.sh
+++ install.sh
@@ -22,47 +22,47 @@ sudo -u $username git clone https://github.com/nash169/linux-config.git /home/$u
 # Install & configure zsh
 sh scripts/modules/zsh.sh $1 $username
 
-# Configure ssh & git
-sh scripts/modules/git.sh $1 $username
+# # Configure ssh & git
+# sh scripts/modules/git.sh $1 $username
 
-# DESkTOP
-sh scripts/modules/desktop.sh $1 $username
+# # DESkTOP
+# sh scripts/modules/desktop.sh $1 $username
 
 # TERMINAL
-sh scripts/modules/terminal.sh $1 $username
+# sh scripts/modules/terminal.sh $1 $username
 
-# EDITOR
-sh scripts/modules/editor.sh $1 $username
+# # EDITOR
+# sh scripts/modules/editor.sh $1 $username
 
-# DEVELOP
-sh scripts/modules/develop.sh $1 $username
+# # DEVELOP
+# sh scripts/modules/develop.sh $1 $username
 
-# EXPLORER
-sh scripts/modules/explorer.sh $1 $username
+# # EXPLORER
+# sh scripts/modules/explorer.sh $1 $username
 
-# BROWSER
-sh scripts/modules/browser.sh $1 $username
+# # BROWSER
+# sh scripts/modules/browser.sh $1 $username
 
-# MULTIMEDIA
-sh scripts/modules/media.sh $1 $username
+# # MULTIMEDIA
+# sh scripts/modules/media.sh $1 $username
 
-# READER
-sh scripts/modules/reader.sh $1 $username
+# # READER
+# sh scripts/modules/reader.sh $1 $username
 
-# EMAIL
-sh scripts/modules/email.sh $1 $username
+# # EMAIL
+# sh scripts/modules/email.sh $1 $username
 
-# SOUND
-sh scripts/modules/sound.sh $1 $username
+# # SOUND
+# sh scripts/modules/sound.sh $1 $username
 
-# BLUETOOTH
-sh scripts/modules/bluetooth.sh $1 $username
+# # BLUETOOTH
+# sh scripts/modules/bluetooth.sh $1 $username
 
-# DOWNLOAD
-sh scripts/modules/download.sh $1 $username
+# # DOWNLOAD
+# sh scripts/modules/download.sh $1 $username
 
-#
-sed -i '/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/s/^/#/g' /etc/sudoers
+# #
+# sed -i '/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/s/^/#/g' /etc/sudoers
 
 # #  Node support
 # pkginstall nodejs npm
