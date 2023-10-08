a66d9f047ce6ce5f672ccb0870db887638e1a828
Bernardo <bernardo.fichera@gmail.com>
2023-10-08 17:38:35 +0200

    Removed title above nvim-tree.

--- configs/neovim/.config/nvim/lua/plugins/bufferline.lua
+++ configs/neovim/.config/nvim/lua/plugins/bufferline.lua
@@ -7,7 +7,7 @@ return {
 			offsets = {
 				{
 					filetype = "NvimTree",
-					text = "File Explorer",
+					-- text = "File Explorer",
 					text_align = "center",
 					separator = true,
 				},
