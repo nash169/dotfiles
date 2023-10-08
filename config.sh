1e2437e043fa67e388b09638e9ad26455fe8ee08
Bernardo <bernardo.fichera@gmail.com>
2023-10-08 17:31:03 +0200

    New script for configuring linux.

---
+++ config.sh
@@ -0,0 +1,16 @@
+
+
+
+
+# install tiling window manager
+# DWM="/home/bernardo/devs/dwm"
+# git clone git@github.com:nash169/dwm-tmp.git $DWM 
+# git -C $DWM remote add upstream git://git.suckless.org/dwm 
+# git -C $DWM fetch upstream
+# git -C $DWM merge upstream/master
+
+ST="/home/bernardo/devs/st"
+git clone git@github.com:nash169/st-tmp.git $ST 
+git -C $ST remote add upstream git://git.suckless.org/st 
+git -C $ST fetch upstream
+git -C $ST merge upstream/master
