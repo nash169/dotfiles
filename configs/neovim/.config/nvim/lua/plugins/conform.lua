2f6b121c9b2f13b6075105faf163faf6d878b430
Bernardo <bernardo.fichera@gmail.com>
2023-10-12 19:03:24 +0200

    Added formatting and linting to neovim.

---
+++ configs/neovim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,27 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				python = { "autopep8" },
+				cpp = { "clang_format" },
+				lua = { "stylua" },
+			},
+
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			require("conform").format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
