5ca078879ec535e4613224112a478e25d0ab20b1
Bernardo <bernardo.fichera@gmail.com>
2023-10-17 12:37:43 +0200

    Fixed some keys and adjusted formatting.

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,41 +1,31 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	
-	config = function()
-		require'nvim-treesitter.configs'.setup {
-			-- A list of parser names, or "all" (the five listed parsers should always be installed)
-			ensure_installed = { "c", "cpp", "python", "lua" },
-			
-			-- Automatically install missing parsers when entering buffer
-			auto_install = true,
-			
-			-- List of parsers to ignore installing (or "all")
-			ignore_install = { "latex" },
-			
-			highlight = {
-				enable = true,
-			
-				-- list of language that will be disabled
-				disable = { "latex" },
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
 
-				-- disable slow treesitter highlight for large files
-				disable = function(lang, buf)
-					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
-			},
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            -- A list of parser names, or "all" (the five listed parsers should always be installed)
+            ensure_installed = { "c", "cpp", "python", "lua" },
 
-			-- enable indentation
-			indent = {
-				enable = true,
-				disable = { "cpp" },
-			}
-		}
+            -- Automatically install missing parsers when entering buffer
+            auto_install = true,
 
-	end,
+            -- List of parsers to ignore installing (or "all")
+            ignore_install = { "latex" },
+
+            highlight = {
+                enable = true,
+
+                -- list of language that will be disabled
+                disable = { "latex" },
+            },
+
+            -- enable indentation
+            indent = {
+                enable = true,
+                disable = { "cpp" },
+            },
+        })
+    end,
 }
