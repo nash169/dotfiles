bbbb0180956ca130e43aa13ebe58bf0998480daa
Bernardo <bernardo.fichera@gmail.com>
2023-11-04 14:40:17 +0100

    Some keymap to navigate through git hunks and reset them.

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,5 +1,17 @@
 return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    config = true,
+    config = function()
+        require("gitsigns").setup({
+            on_attach = function(bufnr)
+                local gs = package.loaded.gitsigns
+                local map = vim.keymap.set
+
+                map("n", "<leader>hn", gs.next_hunk)
+                map("n", "<leader>hp", gs.prev_hunk)
+                map("n", "<leader>hr", gs.reset_hunk)
+                map("n", "<leader>lb", gs.blame_line)
+            end,
+        })
+    end,
 }
