cb80d534d18ce2625c2b94b0ca57b6dcce0cc656
Bernardo <bernardo.fichera@gmail.com>
2024-11-03 15:32:33 +0100

    Disabled wrap by default.

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -4,7 +4,7 @@ local opt = vim.opt
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 vim.schedule(function()
-	opt.clipboard = "unnamedplus"
+    opt.clipboard = "unnamedplus"
 end)
 
 -- show relative line numbers
@@ -12,6 +12,9 @@ opt.relativenumber = true
 
 -- shows absolute line number on cursor line (when relative number is on)
 opt.number = true
+
+--- disable wrap
+opt.wrap = false
 
 -- 2 spaces for indent width
 opt.shiftwidth = 4
@@ -26,9 +29,9 @@ vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
