c14f2e120a058109e49cb61a082626f365679bc2
Bernardo Fichera <bernardo@Bernardos-MacBook-Pro.local>
2025-10-14 10:09:53 +0200

    Auto activate spell check for tex files.

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -16,7 +16,7 @@ vim.g.have_nerd_font = true
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
+    vim.opt.clipboard = "unnamedplus"
 end)
 
 --- disable wrap
@@ -78,3 +78,12 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+-- Enable spell checking automatically for TeX files
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "tex",
+    callback = function()
+        vim.opt_local.spell = true
+        vim.opt_local.spelllang = "en_us"
+    end,
+})
