2bbee9df75b88e7c9ace787fd81c79fa2cf47f80
Bernardo <bernardo.fichera@gmail.com>
2025-11-14 17:20:59 +0100

    fixed conflicting keymaps

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -4,24 +4,24 @@
 --
 -- See `:help gitsigns` to understand what the configuration keys do
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
-	"lewis6991/gitsigns.nvim",
-	-- event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("gitsigns").setup({
-			-- signs = {
-			-- 	add = { text = "+" },
-			-- 	change = { text = "~" },
-			-- 	delete = { text = "_" },
-			-- 	topdelete = { text = "‾" },
-			-- 	changedelete = { text = "~" },
-			-- },
-			on_attach = function(bufnr)
-				local gs = package.loaded.gitsigns
-				vim.keymap.set("n", "<A-j>", gs.next_hunk, { desc = "Go to next hunk" })
-				vim.keymap.set("n", "<A-k>", gs.prev_hunk, { desc = "Go to previous hunk" })
-				vim.keymap.set("n", "<leader>hr", gs.reset_hunk, { desc = "[H]unk [R]eset" })
-				vim.keymap.set("n", "<leader>hh", gs.preview_hunk, { desc = "[H]unk [P]review" })
-			end,
-		})
-	end,
+    "lewis6991/gitsigns.nvim",
+    -- event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        require("gitsigns").setup({
+            -- signs = {
+            -- 	add = { text = "+" },
+            -- 	change = { text = "~" },
+            -- 	delete = { text = "_" },
+            -- 	topdelete = { text = "‾" },
+            -- 	changedelete = { text = "~" },
+            -- },
+            on_attach = function(bufnr)
+                local gs = package.loaded.gitsigns
+                vim.keymap.set("n", "<leader>hn", gs.next_hunk, { desc = "[H]unk [N]ext" })
+                vim.keymap.set("n", "<leader>hp", gs.prev_hunk, { desc = "[H]unk [P]revious" })
+                vim.keymap.set("n", "<leader>hr", gs.reset_hunk, { desc = "[H]unk [R]eset" })
+                vim.keymap.set("n", "<leader>hh", gs.preview_hunk, { desc = "[H]unk [P]review" })
+            end,
+        })
+    end,
 }
