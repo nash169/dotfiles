cd567e0d4545be563f9b3af48b5252310d62c671
Bernardo <bernardo.fichera@gmail.com>
2026-01-21 23:14:40 +0100

    added image support neovim

---
+++ nvim/.config/nvim/lua/plugins/image.lua
@@ -0,0 +1,9 @@
+return {
+    "3rd/image.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        require("image").setup({
+            backend = "ueberzug", -- auto-detects ueberzugpp
+        })
+    end,
+}
