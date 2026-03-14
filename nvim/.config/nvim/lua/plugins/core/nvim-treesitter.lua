b88ef031a97ef83e73c047419f3713b19d545107
Bernardo Fichera <bernardo@Bernardos-MacBook-Pro.fritz.box>
2026-03-14 12:51:13 +0100

    reverted treesitter config changes

--- nvim/.config/nvim/lua/plugins/core/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/core/nvim-treesitter.lua
@@ -1,28 +1,27 @@
-return {
+return { -- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"bash",
-				"c",
-				"cpp",
-				"python",
-				"diff",
-				"html",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"vim",
-				"vimdoc",
-			},
-
-			highlight = {
-				enable = true,
-			},
+		local filetypes = {
+			"bash",
+			"c",
+			"cpp",
+			"python",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"query",
+			"vim",
+			"vimdoc",
+		}
+		require("nvim-treesitter").install(filetypes)
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = filetypes,
+			callback = function()
+				vim.treesitter.start()
+			end,
 		})
 	end,
 }
