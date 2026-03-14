d11dc9a8cd68a463c0d14469c44304f0d2dbee82
Bernardo Fichera <bernardo@Bernardos-MacBook-Pro.fritz.box>
2026-03-14 09:32:02 +0100

    formatting

--- nvim/.config/nvim/lua/plugins/image.lua
+++ nvim/.config/nvim/lua/plugins/image.lua
@@ -1,9 +1,9 @@
 return {
-    "3rd/image.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        require("image").setup({
-            backend = "ueberzug", -- auto-detects ueberzugpp
-        })
-    end,
+	"3rd/image.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("image").setup({
+			backend = "ueberzug", -- auto-detects ueberzugpp
+		})
+	end,
 }
