834dfe755c4bf40f1c6cbeef44926f5c5282f50d
Bernardo Fichera <bernardo@bernardos-mbp.mvl1.uni-tuebingen.de>
2026-03-18 10:18:20 +0100

    moved yank highlight to options

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -94,6 +94,17 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
 -- Diagnostic Config
 -- See :help vim.diagnostic.Opts
 vim.diagnostic.config({
